dd1c445259730a413f27d32fcb220f2db9a1725a
Фукиш <egor158741@yandex.ru>
2023-04-28 18:16:21 +0300

    Add TLM Decoder

--- src/New/geoscan-Decoder-UB1QBJ.py
+++ src/New/geoscan-Decoder-UB1QBJ.py
@@ -1,4 +1,5 @@
 import sys, socket, argparse, time, bitstring
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--port", help="port")
@@ -27,6 +28,44 @@ def start_socket(ip, port):
     print("")
     return s
 
+def telemetry_decoder(data):
+    data=data[32:]
+    time_unix=datetime.utcfromtimestamp(int(bitstring.BitStream(hex=data[:8]).read('uintle'))).strftime('%Y-%m-%d %H:%M:%S')
+    current=int(bitstring.BitStream(hex=data[8:12]).read('uintle'))*0.0000766
+    current_pannels=int(bitstring.BitStream(hex=data[12:16]).read('uintle'))*0.00003076
+    v_oneakb=int(bitstring.BitStream(hex=data[16:20]).read('uintle'))*0.00006928
+    v_akball=int(bitstring.BitStream(hex=data[20:24]).read('uintle'))*0.00013856
+    t_x_p=bitstring.BitStream(hex=data[24:26]).read('int')
+    t_x_n=bitstring.BitStream(hex=data[26:28]).read('int')
+    t_y_p=bitstring.BitStream(hex=data[28:30]).read('int')
+    t_y_n=bitstring.BitStream(hex=data[30:32]).read('int')
+    t_z_p=bitstring.BitStream(hex=data[32:34]).read('int')
+    t_z_n=bitstring.BitStream(hex=data[34:36]).read('int')
+    t_bat1=bitstring.BitStream(hex=data[36:38]).read('int')
+    t_bat2=bitstring.BitStream(hex=data[38:40]).read('int')
+    cpu=bitstring.BitStream(hex=data[40:42]).read('int')*0.390625
+    obc=int(bitstring.BitStream(hex=data[42:46]).read('intle'))-7476
+    commu=int(bitstring.BitStream(hex=data[46:50]).read('intle'))-1505
+    rssi=int(bitstring.BitStream(hex=data[50:52]).read('int'))-99
+    with open('tlm.txt', 'w') as out_tlm_file:
+        out_tlm_file.write(str(time_unix)+'\n')
+        out_tlm_file.write(str(round(float(current), 2))+' A'+'\n')
+        out_tlm_file.write(str(round(float(current_pannels), 2))+' A'+'\n')
+        out_tlm_file.write(str(round(float(v_oneakb), 2))+' V'+'\n')
+        out_tlm_file.write(str(round(float(v_akball), 2))+' V'+'\n')
+        out_tlm_file.write(str(t_x_p)+' C'+'\n')
+        out_tlm_file.write(str(t_x_n)+' C'+'\n')
+        out_tlm_file.write(str(t_y_p)+' C'+'\n')
+        out_tlm_file.write(str(t_y_n)+' C'+'\n')
+        out_tlm_file.write(str('BROKEN')+'\n')
+        out_tlm_file.write(str(t_z_n)+' C'+'\n')
+        out_tlm_file.write(str(t_bat1)+' C'+'\n')
+        out_tlm_file.write(str(t_bat2)+' C'+'\n')
+        out_tlm_file.write(str(round(float(cpu), 2))+'%'+'\n')
+        out_tlm_file.write(str(obc)+'\n')
+        out_tlm_file.write(str(commu)+'\n')
+        out_tlm_file.write(str(rssi)+'\n')
+
 def main(s):
     while True:
         frame = s.recv(2048).hex()
@@ -50,7 +89,6 @@ def main(s):
             chb2=frame[18:20]
             chb3=frame[15:17]
             check_value=bitstring.BitStream(hex=str(str(chb1)+str(chb2)+str(chb3))).read('uint')
-            #print('reg: '+str(reg)+' check_value: '+str(check_value))
             if(int(check_value)==int(reg+56)):
                 with open('out_iamge_'+str(name)+'.jpg', 'ab') as out_file:
                     bitstring.BitArray(hex=str(str(frame[23:]).replace(' ', ''))).tofile(out_file)
@@ -63,6 +101,9 @@ def main(s):
         if(int(str(frame[23:].find(' ff d9 '))) >= int(0)):
             with open('out_iamge_'+str(name)+'.jpg', 'ab') as out_file:
                 bitstring.BitArray(hex=str(str(frame[23:]).replace(' ', ''))).tofile(out_file)
+        if(str(frame[:18]) == str('84 8a 82 86 9e 9c ')):
+            telemetry_decoder(data=str(str(frame).replace(' ', '')))
+        
 
 if(__name__=='__main__'):
     ip=parser.parse_args().ip
